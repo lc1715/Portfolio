8bfc8bab6db2a2f32fbd2f9dfd1f897c9ad48a65
Lisa Chan <lc131715@gmail.com>
2025-04-04 00:54:57 -0700

    add about me section

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -2,8 +2,21 @@
 
 export default function About() {
     return (
-        <section id="About">
+        <section id="About" className="flex flex-col justify-center items-center pt-32">
+            <h2 className="text-4xl md:text-5xl">About Me</h2>
 
-        </section >
+            <div className="flex flex-wrap justify-center gap-x-[100px] pt-9 md:pt-12 lg:pt-[60px]">
+                <img
+                    className="rounded-full object-cover w-72 h-72 md:w-[312px] md:h-[312px]"
+                    src="/self.jpg"
+                    alt="Picture of Lisa Chan"
+                />
+                <div className="flex justify-center text-lg max-w-lg pt-8 lg:pt-0 px-8 lg:px-2">
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, at quae consequuntur doloremque ex nam maiores velit nostrum nemo nisi?
+                    </p>
+                </div>
+            </div >
+        </section>
     );
 }
