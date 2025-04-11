132570ac3996b91f3463fde0dbea362940c7dbae
Lisa Chan <lc131715@gmail.com>
2025-04-10 22:14:54 -0700

    add project data

---
+++ src/app/projectData.js
@@ -0,0 +1,39 @@
+const projectData = [
+    {
+        title: 'BookMarker',
+        imageUrl: '/public/self',
+        imageAlt: 'project screenshot',
+        description: 'project description',
+        techUsed: ['React', 'Express'],
+        links: [
+            { name: 'Frontend Code', url: 'https://github.com/lc1715/BookMarker-Frontend' },
+            { name: 'Backend Code', url: 'https://github.com/lc1715/BookMarker-Backend' },
+            { name: 'Demo', url: 'https://bookmarker-tbif.onrender.com/' }
+        ]
+    },
+    {
+        title: 'Recipe Finder',
+        imageUrl: '/public/self',
+        imageAlt: 'project screenshot',
+        description: 'project description',
+        techUsed: ['React', 'Express'],
+        links: [
+            { name: 'Code', url: 'https://github.com/lc1715/Recipe-Finder' },
+            { name: 'Demo', url: 'https://recipe-finder-kigw.onrender.com/' }
+        ]
+    },
+    {
+        title: 'Upstream',
+        imageUrl: '/public/self',
+        imageAlt: 'project screenshot',
+        description: 'project description',
+        techUsed: ['React', 'Express'],
+        links: [
+            { name: 'Frontend Code', url: 'https://github.com/lc1715/BookMarker-Frontend' },
+            { name: 'Backend Code', url: 'https://github.com/lc1715/BookMarker-Backend' },
+            { name: 'Demo', url: 'https://bookmarker-tbif.onrender.com/' }
+        ]
+    },
+]
+
+export default projectData;
