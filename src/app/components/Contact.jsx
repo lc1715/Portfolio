e7fffe72222e1d94405729e48a85d23586e0b66c
Lisa Chan <lc131715@gmail.com>
2025-04-16 16:43:46 -0700

    add contact section

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,9 +1,77 @@
 'use client'
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contact() {
-    return (
-        <section id="Contact">
+    const [state, handleSubmit] = useForm("xgvabvje");
+    if (state.succeeded) {
+        return (
+            <div className="flex justify-center mt-20">
+                <p>Thanks for contacting me!</p>
+            </div>
+        );
+    }
 
+    return (
+        <section id="Contact" className="flex flex-col justify-center items-center pt-30">
+            <h2 className="text-4xl md:text-5xl">Contact</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-16 mt-10 md:mt-12 mx-4 md:mx-6 mb-3">
+                <div className="max-w-lg px-8">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto saepe expedita! Dignissimos sit assumenda nemo suscipit ducimus, dolorum illum quasi aspernatur.
+                        Rem, quos! Harum fugiat accusamus nulla non aut!
+                    </p>
+                </div>
+                <div className="max-w-lg p-6 rounded-lg border-2 border-gray-700">
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email" className="block">
+                            Email Address
+                        </label>
+                        <input
+                            id='email'
+                            name='email'
+                            type='email'
+                            className="border-b-2 border-gray-700 mb-2 p-2 w-full"
+                        />
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
+                        <label htmlFor='subject' className="block">
+                            Subject
+                        </label>
+                        <input
+                            id='subject'
+                            name='subject'
+                            type='text'
+                            className="border-b-2 border-gray-700 w-full mb-2 p-2"
+                        />
+                        <ValidationError
+                            prefix="Subject"
+                            field="subject"
+                            errors={state.errors}
+                        />
+                        <label htmlFor="message" className="block">
+                            Message
+                        </label>
+                        <textarea
+                            id='message'
+                            name='message'
+                            className="border-b-2 border-gray-700 w-full mb-2 p-2"
+                        />
+                        <ValidationError
+                            prefix="Message"
+                            field="message"
+                            errors={state.errors}
+                        />
+                        <div className="flex justify-center mt-3">
+                            <button type='submit' disabled={state.submitting} className="bg-gray-200 text-stone-950 py-2 px-4 font-semibold rounded-full hover:bg-blue-500 hover:text-white">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </section>
     );
-}
+} 
