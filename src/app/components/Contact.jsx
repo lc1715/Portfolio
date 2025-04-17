31e89ac6ceb3fca7db8c9866a48c8ea24291a692
Lisa Chan <lc131715@gmail.com>
2025-04-16 17:34:03 -0700

    move form submitted

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -4,13 +4,6 @@ import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contact() {
     const [state, handleSubmit] = useForm("xgvabvje");
-    if (state.succeeded) {
-        return (
-            <div className="flex justify-center mt-20">
-                <p>Thanks for contacting me!</p>
-            </div>
-        );
-    }
 
     return (
         <section id="Contact" className="flex flex-col justify-center items-center pt-30">
@@ -23,57 +16,63 @@ export default function Contact() {
                         Rem, quos! Harum fugiat accusamus nulla non aut!
                     </p>
                 </div>
-                <div className="max-w-lg p-6 rounded-lg border-2 border-gray-700">
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="email" className="block">
-                            Email Address
-                        </label>
-                        <input
-                            id='email'
-                            name='email'
-                            type='email'
-                            className="border-b-2 border-gray-700 mb-2 p-2 w-full"
-                            required
-                        />
-                        <ValidationError
-                            prefix="Email"
-                            field="email"
-                            errors={state.errors}
-                        />
-                        <label htmlFor='subject' className="block">
-                            Subject
-                        </label>
-                        <input
-                            id='subject'
-                            name='subject'
-                            type='text'
-                            className="border-b-2 border-gray-700 w-full mb-2 p-2"
-                            required
-                        />
-                        <ValidationError
-                            prefix="Subject"
-                            field="subject"
-                            errors={state.errors}
-                        />
-                        <label htmlFor="message" className="block">
-                            Message
-                        </label>
-                        <textarea
-                            id='message'
-                            name='message'
-                            className="border-b-2 border-gray-700 w-full mb-2 p-2"
-                            required
-                        />
-                        <ValidationError
-                            prefix="Message"
-                            field="message"
-                            errors={state.errors}
-                        />
-                        <div className="flex justify-center mt-3">
-                            <button type='submit' disabled={state.submitting} className="bg-gray-200 text-stone-950 py-2 px-4 font-semibold rounded-full hover:bg-blue-500 hover:text-white">Submit</button>
-                        </div>
-                    </form>
-                </div>
+                {state.succeeded ?
+                    <div className="flex justify-center items-center">
+                        <p>Thanks for contacting me!</p>
+                    </div>
+                    :
+                    <div className="max-w-lg p-6 rounded-lg border-2 border-gray-700">
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="email" className="block">
+                                Email Address
+                            </label>
+                            <input
+                                id='email'
+                                name='email'
+                                type='email'
+                                className="border-b-2 border-gray-700 mb-2 p-2 w-full"
+                                required
+                            />
+                            <ValidationError
+                                prefix="Email"
+                                field="email"
+                                errors={state.errors}
+                            />
+                            <label htmlFor='subject' className="block">
+                                Subject
+                            </label>
+                            <input
+                                id='subject'
+                                name='subject'
+                                type='text'
+                                className="border-b-2 border-gray-700 w-full mb-2 p-2"
+                                required
+                            />
+                            <ValidationError
+                                prefix="Subject"
+                                field="subject"
+                                errors={state.errors}
+                            />
+                            <label htmlFor="message" className="block">
+                                Message
+                            </label>
+                            <textarea
+                                id='message'
+                                name='message'
+                                className="border-b-2 border-gray-700 w-full mb-2 p-2"
+                                required
+                            />
+                            <ValidationError
+                                prefix="Message"
+                                field="message"
+                                errors={state.errors}
+                            />
+                            <div className="flex justify-center mt-3">
+                                <button type='submit' disabled={state.submitting} className="bg-gray-200 text-stone-950 py-2 px-4 font-semibold rounded-full hover:bg-blue-500 hover:text-white">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                }
             </div>
         </section>
     );
