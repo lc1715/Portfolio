68d2e4b0cccbbe73432e0429094321243e60b871
Lisa Chan <lc131715@gmail.com>
2025-05-11 04:57:48 -0700

    update navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -11,13 +11,13 @@ export default function Navbar() {
     return (
         <nav className="border-b-4 border-gray-500 bg-background fixed top-0 left-0 w-full">
             <div className="flex flex-col justify-center items-center md:flex-row md:justify-between relative min-h-16 px-5 py-2 md:py-0 ">
-                <h1 className="text-lg font-bold">
+                <h1 className="text-lg md:font-bold">
                     <a href="#About" className="transition duration-100 hover:bg-gradient-to-r from-blue-300 to-indigo-300 hover:text-transparent bg-clip-text">
                         Lisa︱Full Stack Developer
                     </a>
                 </h1>
 
-                <ul className="flex flex-wrap justify-center md:justify-between gap-x-5 pt-1 md:pt-0 md:px-2">
+                <ul className="flex flex-wrap justify-center md:justify-between gap-x-5 py-1 md:py-0 md:px-2">
                     {links.map((l, idx) => (
                         <li key={idx}>
                             <a href={l.href} className="transition duration-100 hover:bg-gradient-to-r from-blue-300 to-indigo-300 hover:text-transparent bg-clip-text">
